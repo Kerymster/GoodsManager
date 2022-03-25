14809aa80b083a2ead59248379711a1a040ab7bf
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-26 00:15:52 +0300

    end of UI

--- src/Components/CategoryPanel/CategoryPanel.jsx
+++ src/Components/CategoryPanel/CategoryPanel.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./CategoryPanel.css";
 import BookOutlinedIcon from "@material-ui/icons/BookOutlined";
 import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
+
 const CategoryPanel = () => {
+  let item = false;
   return (
     <div className="cat-card">
       <div className="top">
@@ -14,8 +16,23 @@ const CategoryPanel = () => {
         </div>
       </div>
       <div className="mid">
-        <AddCircleOutlineRoundedIcon className="plus-icon" />
-        <p>Select Your Goods To Manage Them!</p>
+        {item ? (
+          <>
+            <AddCircleOutlineRoundedIcon className="plus-icon" />
+            <p>Select Your Goods To Manage Them!</p>
+          </>
+        ) : (
+          <div className="card">
+            <ul>
+              <li>
+                <div className="inputGroup">
+                  <label className="inputLabel">Item</label>
+                  <input className="inputField" type="checkbox" />
+                </div>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
       <div className="bottom">
         <div className="prd-btns">
